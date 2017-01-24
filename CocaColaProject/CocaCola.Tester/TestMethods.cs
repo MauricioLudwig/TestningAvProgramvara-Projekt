4c4fa65275f695768d7f888c5f47322f7b193e69
Housame <housame.oueslati@hotmail.com>
2017-01-24 11:20:42 +0100

    Some comments fix

--- CocaColaProject/CocaCola.Tester/TestMethods.cs
+++ CocaColaProject/CocaCola.Tester/TestMethods.cs
@@ -13,6 +13,7 @@ namespace CocaCola.Tester
     {
         // Create an instance to test:  
         NumOperator numOp = new NumOperator();
+        #region Test input 1
         [Test]
         public void Test_Input1_Return1()
         //Test the output of progrem when the number is 1
@@ -26,9 +27,11 @@ namespace CocaCola.Tester
             // Assert: Verify the result
             Assert.AreEqual(expectedOutput, actualOutput);
         }
+        #endregion
 
+        #region Sequential Test that test many inputs with many outputs
         [Test, Sequential]
-        public void Input2_Return2(
+        public void Input_MoreThanOne_Return_ManyAlternatives(
             [Values(64, 36, 50, 2, 15)]int input,
             [Values("64", "Coca", "Cola","2", "CocaCola")] string expectedResult)
         //Test the output of progrem when the number is 
@@ -38,9 +41,11 @@ namespace CocaCola.Tester
             // Assert: Verify the result
             Assert.AreEqual(expectedResult, actualOutput);
         }
+        #endregion
 
+        #region Test input that return Coca
         [Test]
-        public void Input3_ReturnCoca(
+        public void Input_That_Return_Coca(
             [Values(3, 9, 12, 18, 21)] int input)
         //Test the output of progrem when the number is 
         {
@@ -51,9 +56,11 @@ namespace CocaCola.Tester
             // Assert: Verify the result
             Assert.AreEqual(expectedResult, actualOutput);
         }
+        #endregion
 
+        #region Test input that return Cola
         [Test]
-        public void Test_Input5_Return_Cola(
+        public void Test_Input_That_Return_Cola(
             [Values(5, 10, 20, 25)] int input)
         //Test the output of progrem when the number is 
         {
@@ -66,9 +73,11 @@ namespace CocaCola.Tester
             // Assert: Verify the result
             Assert.AreEqual(expectedOutput, actualOutput);
         }
+        #endregion
 
+        #region Test input that return CocaCola
         [Test]
-        public void Test_Input15_Return_CocaCola(
+        public void Test_Input_That_Return_CocaCola(
             [Values(15, 30, 45, 60, 75, 90)] int input)
         //Test the output of program when the number is 
         {
@@ -81,5 +90,6 @@ namespace CocaCola.Tester
             // Assert: Verify the result
             Assert.AreEqual(expectedOutput, actualOutput);
         }
+        #endregion
     }
 }
