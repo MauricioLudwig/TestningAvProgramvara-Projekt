771fd1442fb9404c8c4ded55fc19b64fadd2864d
MauricioLudwig <mrdamiengallagher@gmail.com>
2017-01-24 11:07:34 +0100

    Comments added.

--- CocaColaProject/CocaColaProject/Runtime.cs
+++ CocaColaProject/CocaColaProject/Runtime.cs
@@ -15,14 +15,18 @@ namespace CocaColaProject
         public void Start()
         {
 
-            // Creating a new instance of NumOperator to accesss its methods. NumOpeator derives from a class library
+            // Creating a new instance of NumOperator to access its methods. NumOpeator derives from a class library
             NumOperator numOp = new NumOperator();
 
+            // Region is comprised of a for loop which increments the I variable from 1-100, which itself serves as the input
+            // for the NumOperator. The NumOperator returns a string which is then printed out onto the console
+            #region PrintOperator
             for (int i = 1; i < 101; i++)
             {
                 // Invoking the CocaColaOperator for each increment of I. The string returned is then printed to the Console
                 Console.WriteLine(numOp.CocaColaOperator(i));
             }
+            #endregion
 
             Console.ReadKey();
         }
