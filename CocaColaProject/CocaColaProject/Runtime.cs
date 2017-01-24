45bb87e79d2d33e427a34c80a6f52b042b3d5cb6
MauricioLudwig <mrdamiengallagher@gmail.com>
2017-01-24 11:04:31 +0100

    Comments added in Runtime class

--- CocaColaProject/CocaColaProject/Runtime.cs
+++ CocaColaProject/CocaColaProject/Runtime.cs
@@ -7,16 +7,20 @@ using NumberOperator.Library;
 
 namespace CocaColaProject
 {
+
+    // Creating a new class Runtime so that we may avoid any issues in dealing with static methods
     class Runtime
     {
-        
+       
         public void Start()
         {
 
+            // Creating a new instance of NumOperator to accesss its methods. NumOpeator derives from a class library
             NumOperator numOp = new NumOperator();
 
             for (int i = 1; i < 101; i++)
             {
+                // Invoking the CocaColaOperator for each increment of I. The string returned is then printed to the Console
                 Console.WriteLine(numOp.CocaColaOperator(i));
             }
 
